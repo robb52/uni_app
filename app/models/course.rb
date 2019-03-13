e3212746c8f32652bf969dd8d053bf03f3d33576
Rob Brown <brown4homes@yahoo.com>
2019-03-13 16:54:43 -0700

    Add validation for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-
+  validates :name, presence: true, length: { minium: 5, maximum: 50 }
+  validates :short_name, presence: true, length: { minium: 3, maximum: 15 }
+  validates :description, presence: true, length: { minium: 10, maximum: 300 }
 end
